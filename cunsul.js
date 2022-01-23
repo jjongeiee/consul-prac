f9295744d33ea86f3d880ee07ed88a4d6f1f08f0
jjongeiee <jjongeiee@gmail.com>
2022-01-23 17:41:46 +0900

    ddddd

--- cunsul.js
+++ cunsul.js
@@ -1,2 +1,10 @@
 var youtu = document.getElementById('youtu');
-function disp() {youtu.style.display = "none"};
+function disp(self) {
+    if(self.value === '감추기'){
+    youtu.style.display = "none";
+    self.value = '보이기';
+} else {
+    youtu.style.display = "";
+    self.value = '감추기';
+}
+};
